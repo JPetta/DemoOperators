98abb58b9b967f02cd1176d3ed65a87f12632af4
JPetta <jovipetraratulangi@gmail.com>
2019-04-09 12:01:24 +0700

    first commit

---
+++ Demo_Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Demo_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a = 1;
+            //var b = 2;
+            //var c = 3;
+
+            //console.writeline((a + b) * c);
+
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(c > a && c > b);
+            Console.WriteLine(c > a || c > b);
+            Console.WriteLine(!(c > a || c > b));
+        }
+    }
+}
